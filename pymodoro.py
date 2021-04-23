bd7a3bc844097fc3c9881b9dc8c510d0cb04e2ad
cblac105 <77287083+cblac105@users.noreply.github.com>
2021-04-23 14:27:16 -0400

    Update pymodoro.py
    Added 6 commit changes for 4/23 Sprint 2

--- pymodoro.py
+++ pymodoro.py
@@ -10,13 +10,14 @@ REMAINING_TIME_FRACTION = 0.2
 
 
 def dnd_on():
-    print("DND on")
+    from termcolor import colored
+    print colored ("DND on", attrs=['bold']) #used bold to draw audience's eyes in 4/22
     time.sleep(5)  # Delaying so that our notification about DND ON started appears on screen ;)
     os.system("do-not-disturb on")
 
 
 def dnd_off():
-    print("DND off")
+    print("Do not disturb off") #used to tell readers DND is off 4/22
     os.system("do-not-disturb off")
 
 
@@ -27,7 +28,7 @@ def show_end():
     window_height = 500
     position_right = int(root.winfo_screenwidth() / 2 - window_width / 2)
     position_down = int(root.winfo_screenheight() / 2 - window_height / 2)
-    # Positions the window in the center of the page.
+    # position the window in the center of the page 
     root.geometry("{}x{}+{}+{}".format(window_width, window_height, position_right, position_down))
     label = tk.Label(root, text="Pomodoro finished.\nTake a break!")
     label.place(x=window_width/2, y=window_height/2, anchor="center")
@@ -36,10 +37,10 @@ def show_end():
 
 
 def speak(msg):
-    os.system(msg)
+    os.system("pomodoro has finished, please take a break") #speaking message to the user 4/22
 
 
-def notify(title, text):
+def notify(titlehas, text):
     os.system("""
               osascript -e 'display notification "{}" with title "{}"'
               """.format(text, title))
@@ -74,7 +75,7 @@ try:
 
     # System notification
     # FIXME Experimenting
-    os.system("do-not-disturb off")
+    os.system print colored ("do-not-disturb off",'yellow''bold') #added colors to the ending text to draw audiene eyes 4/22
     notify("Pymodoro", f"Pomodoro: {remaining_minutes} minutes left.")
     time.sleep(5)
     os.system("do-not-disturb on")
@@ -82,10 +83,11 @@ try:
     if sound_on:
         x = threading.Thread(target=speak,
                              args=(f"say {remaining_minutes} minutes left in the pomodoro",))
+                             args=(f"say you can do it!!") #added a motivational statement for the user 4/22
         x.start()
 
     for minute in range(remaining_minutes):
-        print(f"{remaining_minutes - minute} minutes left")
+        print(f"{remaining_minutes - minute} minutes left") 
         time.sleep(SECONDS_IN_A_MINUTE)
 
     print("Pomodoro finished")
@@ -99,4 +101,4 @@ try:
 except (KeyboardInterrupt, SystemExit):
     print("\n")
     dnd_off()
-    print("You've successfully interrupted a pomodoro. Goodbye!")
+    print colored("You've successfully interrupted a pomodoro. Goodbye!",'Yellow'[bold]) #used to draw users eyes 4/22
